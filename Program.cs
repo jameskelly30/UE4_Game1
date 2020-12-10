07ce30682beeaf29d7f9efdf5b903d60a8afa2e8
James K <62737213+jameskelly30@users.noreply.github.com>
2020-12-09 19:01:03 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace kelly_james_app1
+{
+    class MonthGrind
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("This Program Created will tell you how many days that are each Month...Press any key to continue: ");
+            Console.ReadKey();
+            Console.WriteLine("Presented by James Kelly \n");
+            Console.WriteLine("Day/Month Calculator:\n ");
+            Console.WriteLine("Please enter the month number: ");
+            int monthNum = Convert.ToInt32(Console.ReadLine());
+
+            Month month = new Month(monthNum);
+
+            Console.WriteLine(month);
+            Console.ReadKey();  //Press Enter 
+            Console.WriteLine("Thanks! ");
+
+        }
+    }
+}
